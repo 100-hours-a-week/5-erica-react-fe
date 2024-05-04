740e3949b87c9bf0a672a90b2652e3725587b0d8
hyunahrho29@gmail.com <hyunahrho29@gmail.com>
2024-05-05 02:11:31 +0900

    add: headers

--- src/static.js
+++ src/static.js
@@ -1,1 +1,8 @@
 export const backHost = "http://localhost:8000";
+
+export const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "ngrok-skip-browser-warning": "69420",
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
