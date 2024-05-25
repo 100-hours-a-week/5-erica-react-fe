664d478cb68a71eeb4174fdca4f81c44604c7049
hyunahrho29@gmail.com <hyunahrho29@gmail.com>
2024-05-26 02:49:03 +0900

    modify: app

--- src/App.js
+++ src/App.js
@@ -18,26 +18,28 @@ function App() {
     <BrowserRouter>
       <div className={styles.App}>
         <Navbar />
-        <section className={styles.main}>
-          <Routes>
-            <Route path={navUrl.logIn} element={<LogInPage />}></Route>
-            <Route path={navUrl.signUp} element={<SignUpPage />}></Route>
-            <Route path={navUrl.posts} element={<Posts />}></Route>
-            <Route path={navUrl.addPost} element={<AddPost />}></Route>
-            <Route
-              path={navUrl.postDetail}
-              element={<PostDetailPage />}
-            ></Route>
-            <Route path={navUrl.updatePost} element={<UpdatePost />}></Route>
-            <Route
-              path={navUrl.updateProfile}
-              element={<UpdateProfile />}
-            ></Route>
-            <Route
-              path={navUrl.updatePassword}
-              element={<UpdatePassword />}
-            ></Route>
-          </Routes>
+        <section className={styles.container}>
+          <div className={styles.main}>
+            <Routes>
+              <Route path={navUrl.logIn} element={<LogInPage />}></Route>
+              <Route path={navUrl.signUp} element={<SignUpPage />}></Route>
+              <Route path={navUrl.posts} element={<Posts />}></Route>
+              <Route path={navUrl.addPost} element={<AddPost />}></Route>
+              <Route
+                path={navUrl.postDetail}
+                element={<PostDetailPage />}
+              ></Route>
+              <Route path={navUrl.updatePost} element={<UpdatePost />}></Route>
+              <Route
+                path={navUrl.updateProfile}
+                element={<UpdateProfile />}
+              ></Route>
+              <Route
+                path={navUrl.updatePassword}
+                element={<UpdatePassword />}
+              ></Route>
+            </Routes>
+          </div>
         </section>
       </div>
     </BrowserRouter>
