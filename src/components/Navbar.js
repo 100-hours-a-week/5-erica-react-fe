73bc75fc20e8eef0c2151a1c06646243b4139634
hyunahrho29@gmail.com <hyunahrho29@gmail.com>
2024-05-26 02:49:14 +0900

    delete: back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
-import BackButton from "./BackButton";
 import { Link } from "react-router-dom";
 import { navUrl } from "../utils/navigate";
-
+import logo from "../images/logo.png";
 import UserProfile from "./users/UserProfile";
 import styles from "../styles/Navbar.module.css";
 import { useShowProfile } from "../hooks/useShowProfile";
@@ -9,15 +8,10 @@ import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const { pathname } = useLocation();
-  const { showProfile, showBackButton } = useShowProfile(pathname);
+  const { showProfile } = useShowProfile(pathname);
   return (
     <section className={styles.navbar}>
       <div className={styles.navbarContainer}>
-        {showBackButton ? (
-          <BackButton />
-        ) : (
-          <div className={styles.emptyBackButton}></div>
-        )}
         <Link
           className={styles.navbarTitle}
           to={
@@ -26,7 +20,7 @@ export default function Navbar() {
               : navUrl.posts
           }
         >
-          아무 말 대잔치
+          <img alt="로고이미지" src={logo} className={styles.logo} />
         </Link>
         {showProfile ? (
           <UserProfile />
